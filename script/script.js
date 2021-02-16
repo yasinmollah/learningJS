8bba6955e5dbcc3febccf7b2414152b7542a3691
YASIN MOLLAH <yasinmollah96@gmail.com>
2021-02-17 03:24:23 +0600

    Coding challenge 2 has Done Successfully

--- script/script.js
+++ script/script.js
@@ -14,3 +14,55 @@ var higherBMI = johnBMI < markBMI;
 console.log(`Is Mark's BMI higher than John?? ${higherBMI}`);
 */
 
+//Coding Challenge 2
+
+//john's Score
+var johnFirstScore, johnSecondScore, johnThirdScore, johnAvgScore;
+
+johnFirstScore = 89;
+johnSecondScore = 120;
+johnThirdScore = 103;
+
+//step 1 part 1
+johnAvgScore = (johnFirstScore + johnSecondScore + johnThirdScore) / 3;
+
+//Mike's Score
+var mikeFirstScore, mikeSecondScore, mikeThirdScore, mikeAvgScore;
+
+mikeFirstScore = 116;
+mikeSecondScore = 94;
+mikeThirdScore = 123;
+
+//Step 1 part 2
+mikeAvgScore = (mikeFirstScore + mikeSecondScore + mikeThirdScore) / 3;
+
+// Step 2
+johnAvgScore > mikeAvgScore
+    ? console.log(`John is the winner with the average score ${johnAvgScore}`)
+    : console.log(`Mike is the winner with the average score ${mikeAvgScore}`);
+
+//step 3 has tested several times
+
+//Step 4 is testing here
+//Mary's Score
+var maryFirstScore, marySecondScore, maryThirdScore, maryAvgScore;
+
+maryFirstScore = 97;
+marySecondScore = 134;
+maryThirdScore = 105;
+
+maryAvgScore = (maryFirstScore + marySecondScore + maryThirdScore) / 3;
+
+johnAvgScore > mikeAvgScore
+    ? johnAvgScore > maryAvgScore
+        ? console.log(
+              `John is the winner with the average score ${johnAvgScore}`
+          )
+        : console.log(
+              `Mary is the winner with the average score ${maryAvgScore}`
+          )
+    : mikeAvgScore > maryAvgScore
+    ? console.log(`Mike is the winner with the average score ${mikeAvgScore}`)
+    : console.log(`Mary is the winner with the average score ${maryAvgScore}`);
+
+//Step 5 has tested several times
