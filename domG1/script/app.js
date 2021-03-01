936561efc73132f7feb3bfb84d11b89e2e8d1796
YASIN MOLLAH <yasinmollah96@gmail.com>
2021-03-01 12:10:21 +0600

    Event Handler in DOM

--- domG1/script/app.js
+++ domG1/script/app.js
@@ -1,12 +1,24 @@
-var scores, roundScore, dice;
+var scores, roundScore, activePlayer;
 
 score = [0, 0];
 roundScore = 0;
+activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-// console.log(dice);
-document.querySelector("#name-1").textContent = "Yasin";
-document.querySelector("#name-2").textContent = "Mollah";
+// document.querySelector("#current-" + activePlayer).textContent = dice;
 
-document.querySelector("#current-1").textContent = dice;
-document.querySelector("#current-2").textContent = dice;
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-1").textContent = "0";
+document.getElementById("score-2").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+document.getElementById("current-2").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    // 1.Select random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //2.Display the Result
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
+    diceDom.src = "../images/dice-" + dice + ".png";
+    //3.update the round score if the rolled number is not 1
+});
