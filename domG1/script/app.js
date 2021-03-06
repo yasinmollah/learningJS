b31e1047e4a16152b1c471c11bb89ff6adeddc21
YASIN MOLLAH <yasinmollah96@gmail.com>
2021-03-07 03:56:50 +0600

    Successfully completed third part of coding challenge 3

--- domG1/script/app.js
+++ domG1/script/app.js
@@ -1,32 +1,36 @@
-var scores, roundScore, activePlayer, gamePlay, dualSix;
+var scores, roundScore1, roundScore2, activePlayer, gamePlay;
 
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
     if (gamePlay) {
         // 1.Select random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
         //2.Display the Result
-        var diceDom = document.querySelector(".dice");
-        diceDom.style.display = "block";
-        diceDom.src = "../images/dice-" + dice + ".png";
+        var diceDom1 = document.querySelector(".dice1");
+        var diceDom2 = document.querySelector(".dice2");
+        diceDom1.style.display = "block";
+        diceDom2.style.display = "block";
+        diceDom1.src = "../images/dice-" + dice1 + ".png";
+        diceDom2.src = "../images/dice-" + dice2 + ".png";
         // Checking two six in a row [challenge 3 : part 1]
-        dualSix.push(dice);
-        if (dualSix[0] === 6 && dualSix[1] === 6) {
-            roundScore = 0;
+        if (dice1 === 6 && dice2 === 6) {
+            roundScore1 = 0;
+            roundScore2 = 0;
             scores[activePlayer - 1] = 0;
             document.getElementById("current-" + activePlayer).textContent = 0;
             document.getElementById("score-" + activePlayer).textContent = 0;
             nextPlayer();
         } else {
             //3.update the round score if the rolled number is not 1
-            if (dice !== 1) {
+            if (dice1 !== 1 && dice2 !== 1) {
                 // Add Score
-                roundScore += dice;
+                roundScore1 += dice1;
+                roundScore2 += dice2;
                 // Display the score in the current score box
-                document.getElementById(
-                    "current-" + activePlayer
-                ).textContent = roundScore;
+                document.getElementById("current-" + activePlayer).textContent =
+                    roundScore1 + roundScore2;
             } else {
                 // Switch to the next player
                 nextPlayer();
@@ -39,7 +43,7 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 document.querySelector(".btn-hold").addEventListener("click", function () {
     if (gamePlay) {
         //Add current score to global score
-        scores[activePlayer - 1] += roundScore;
+        scores[activePlayer - 1] += roundScore1 + roundScore2;
         //Update the UI
         document.getElementById("score-" + activePlayer).textContent =
             scores[activePlayer - 1];
@@ -52,7 +56,8 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
             document.getElementById("name-" + activePlayer).textContent =
                 "Winner!!";
             //Disappear the dice
-            document.querySelector(".dice").style.display = "none";
+            document.querySelector(".dice1").style.display = "none";
+            document.querySelector(".dice2").style.display = "none";
             //Move the winner player to the winner class
             document
                 .querySelector(`.player-${activePlayer}-pannel`)
@@ -73,11 +78,13 @@ document.querySelector(".btn-new").addEventListener("click", init);
 
 function init() {
     scores = [0, 0];
-    roundScore = 0;
+    roundScore1 = 0;
+    roundScore2 = 0;
     activePlayer = 1;
     gamePlay = true;
-    dualSix = [0, 0];
-    document.querySelector(".dice").style.display = "none";
+    document.querySelector(".dice1").style.display = "none";
+    document.querySelector(".dice2").style.display = "none";
+    document.getElementById("finalScore").value = "";
 
     document.getElementById("score-1").textContent = "0";
     document.getElementById("score-2").textContent = "0";
@@ -97,7 +104,8 @@ function nextPlayer() {
     // switch to the next player
     activePlayer === 1 ? (activePlayer = 2) : (activePlayer = 1);
     // Resetting the current score
-    roundScore = 0;
+    roundScore1 = 0;
+    roundScore2 = 0;
     // Resetting the current score box
     document.querySelector("#current-1").textContent = 0;
     document.querySelector("#current-2").textContent = 0;
@@ -105,5 +113,6 @@ function nextPlayer() {
     document.querySelector(".player-1-pannel").classList.toggle("active");
     document.querySelector(".player-2-pannel").classList.toggle("active");
     // Disappear the dice when the dice value is 1
-    document.querySelector(".dice").style.display = "none";
+    document.querySelector(".dice1").style.display = "none";
+    document.querySelector(".dice2").style.display = "none";
 }
