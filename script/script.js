53fadcc72bd3190670757405ff0a581efc719032
YASIN MOLLAH <yasinmollah96@gmail.com>
2021-03-08 23:03:52 +0600

    Practicing Constructor and instance

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 // Inheritance & Prototype
 
 // Normal Object
-
+/*
 var mrX = {
     name: "X",
     yearOfBirth: 1990,
@@ -14,8 +14,8 @@ var mrX = {
 };
 mrX.calculateAge();
 console.log(mrX);
+*/
 
-/*
 var person = function (name, yearOfBirth, profession) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
@@ -25,9 +25,20 @@ var person = function (name, yearOfBirth, profession) {
 person.prototype.calculateAge = function () {
     this.age = Number(new Date().getFullYear()) - this.yearOfBirth;
 };
+person.prototype.university = "NUBTK";
 
 var yasin = new person("Yasin", 1996, "Student");
 yasin.calculateAge();
 console.log(yasin);
 
-*/
+var prince = new person("Prince", 1995, "Police");
+prince.calculateAge();
+console.log(prince);
+
+var rahina = new person("Rahina", 1997, "Engineer");
+rahina.calculateAge();
+console.log(rahina);
+
+var eity = new person("Eity", 1996, "Magistrate");
+eity.calculateAge();
+console.log(eity);
